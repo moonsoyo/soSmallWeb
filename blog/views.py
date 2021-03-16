16c3855687fd525823520653fe978c8630dfdfdd
Soyoung Moon <moonsoyo@usc.edu>
2021-03-16 20:37:06 +0900

    Making blog post list page using FBV

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('-pk')
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts': posts,  # views.py 에서 데이터베이스에 쿼리를 날려서 원하는 레코드를 가져올 수 있음.
+        }
+    )
