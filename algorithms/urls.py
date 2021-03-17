5a26abd3e43dc66616c02055d48ecb40c5a7c863
Soyoung Moon <moonsoyo@usc.edu>
2021-03-17 18:03:48 +0900

    Created algorithms detail post page using CBV

--- algorithms/urls.py
+++ algorithms/urls.py
@@ -4,5 +4,5 @@ from . import views
 # int:pk 는 정수 형태의 값을 pk라는 변수로 담아 함수로 넘기겠다는 말이다.
 urlpatterns = [
     path('', views.PostList.as_view()),
-    path('<int:pk>/', views.single_post_page),
+    path('<int:pk>/', views.PostDetail.as_view()),
 ]
