b017366a498cfcb4a5d112db73bcdb3dba5a23e3
Soyoung Moon <moonsoyo@usc.edu>
2021-03-19 18:03:37 +0900

    Testing when there is no post in post list page

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
+from .models import Post
 
-# Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_post_list(self):
+        # post list page
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(soup.title.text, 'Blog')
+
+        navbar = soup.nav
+
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        self.assertEqual(Post.objects.count(), 0)
+        main_area = soup.find('div', id='main_area')
+        self.assertIn('No posts yet.', main_area.text)
+
